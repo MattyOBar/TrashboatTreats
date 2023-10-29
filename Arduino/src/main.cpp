e6ce81beb1269fe41c8555ee833f4402ecb54ff2
Matthew OBar <matthewobar@gmail.com>
2023-10-29 18:53:28 -0500

    button to activate stepper motor working

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <Stepper.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Stepper variables & instances
+const int stepsPerRev = 509;
+const int stepperSpeed = 50;
+const int stepperDwell = 300;
+Stepper stepper = Stepper(stepsPerRev, 2, 4, 3, 5);
+
+// main dispenser button
+const int buttonMain = 22;
+
+
+// This function activates the stepper motor to dispense treats
+// stepperSpeed adjusts the speed of the motor
+// stepsPerRev adjusts the length of the rotation
+// stepperDwell adjusts the time dwell inbetween rotations
+void dispenseTreats() {
+	stepper.setSpeed(stepperSpeed);
+	stepper.step(stepsPerRev);
+	delay(stepperDwell);
+
+	stepper.setSpeed(stepperSpeed);
+	stepper.step(-stepsPerRev);
+	delay(stepperDwell);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(9600);
+	pinMode(buttonMain, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	if (digitalRead(buttonMain) == 0) {
+		dispenseTreats();
+	}
+	
+	
+	
 }
